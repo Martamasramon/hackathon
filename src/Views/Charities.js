226d2941082eb3b9c0d25466419cb022f869d676
marta masramon <marta.masramon@gmail.com>
2021-03-06 22:44:04 +0000

    changes

--- src/Views/Charities.js
+++ src/Views/Charities.js
@@ -5,63 +5,59 @@ function Charities() {
   return (
     <div className="Charities">
 
-   <div className="home center">
-       
+      <div className="home center">
         <h2>Charities</h2>
         <p>Here are some of the charities that we work with. All are well known non-profit organisations that fight for causes that are important to us.</p>
       </div>
-    
-    
-  <div className="home yellow">
-  <div class="container">
-      <div class="image">
-        <img className="char-img" src={process.env.PUBLIC_URL + "/images/heartfoundation.png"} alt="logo"></img>
+
+      <div className="home">
+
+        <div class="container">
+          <div class="image">
+            <img className="char-img" src={process.env.PUBLIC_URL + "/images/heartfoundation.png"} alt="logo"></img>
+          </div>
+
+          <div class="text">
+            <h3>British Heart Fundation</h3>
+            <p>
+            <a href="https://www.bhf.org.uk">British Heart Fundation</a>funds research related to heart and circulatory diseases and their risk factors.</p>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="image">
+            <img className="char-img" src={process.env.PUBLIC_URL + "/images/mcmillan.png"} alt="logo"></img>
+          </div>
+
+          <div class="text">
+            <h3>Macmillan Cancer Support</h3>
+            <p><a href="https://www.macmillan.org.uk">Macmillan Cancer Support</a>is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
+          </div>
+        </div>
+
+       <div class="container">
+        <div class="image">
+          <img className="char-img" src={process.env.PUBLIC_URL + "/images/StJohn.png"} alt="logo"></img>
+        </div>
+
+        <div class="text">
+          <h3>St. John Ambulance</h3>
+          <p><a href="https://www.sja.org.uk">St John Ambulance</a>is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
+       </div>
       </div>
-      <div class="text">
-        <h3>British Heart Fundation</h3>
-          <p>
-    <a href="https://www.bhf.org.uk">British Heart Fundation</a>funds research related to heart and circulatory diseases and their risk factors.</p>
-      </div>
-    </div>
-    
- 
-    <div class="container">
-    <div class="image">
-    <img className="char-img" src={process.env.PUBLIC_URL + "/images/mcmillan.png"} alt="logo"></img>
-      </div>
-      <div class="text">
-        <h3>Macmillan Cancer Support</h3>
-        <p>
-    <a href="https://www.macmillan.org.uk">Macmillan Cancer Support</a>is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
-      </div>
-    </div>
-    </div>
-    
-     <div class="container">
-    <div class="image">
-    <img className="char-img" src={process.env.PUBLIC_URL + "/images/StJohn.png"} alt="logo"></img>
-      </div>
-      <div class="text">
-        <h3>St. John Ambulance</h3>
-      <p>
-    <a href="https://www.sja.org.uk">St John Ambulance</a>is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
-     </div>
-    </div>
-    
-    
-    
-     <div class="container">
-    <div class="image">
-    <img className="char-img" src={process.env.PUBLIC_URL + "/images/Keep.png"} alt="logo"></img>
-      </div>
-      <div class="text">
-        <h3>Keep Britain Tidy</h3>
-        <p>
-    <a href="https://www.keepbritaintidy.org">Keep Britain Tidy</a>is a UK-based independent environmental charity. The organisation campaigns to reduce litter, improve local places and prevent waste. It has offices in Wigan and London.</p>
+
+       <div class="container">
+        <div class="image">
+          <img className="char-img" src={process.env.PUBLIC_URL + "/images/Keep.png"} alt="logo"></img>
+        </div>
+
+        <div class="text">
+          <h3>Keep Britain Tidy</h3>
+          <p><a href="https://www.keepbritaintidy.org">Keep Britain Tidy</a>is a UK-based independent environmental charity. The organisation campaigns to reduce litter, improve local places and prevent waste. It has offices in Wigan and London.</p>
+        </div>
       </div>
     </div>
-    </div>
-    
+
     </div>
   );
 }
--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -3,12 +3,19 @@ import React from "react";
 
 function WhoAreWe() {
   return (
-    <div className="whoarewe center">
-      <h1>Who Are We?</h1>
-       <img className="us-img" src={process.env.PUBLIC_URL + "/images/US.png"} alt="5 happy girls"></img>
-       <p>From left to right: Marta Masramon Munoz, Mireia Munoz Rojo, Livia Soro, Manuela Giraud, Yacout Rtel Bennani</p>
-      <p>We are five bioengineering students from Imperial College. We decided to create Seedling because we wanted to make recycling easier for everyone.</p>
-      <p>We created Seedling during a 24h Hackathon hosted by the Women in Sciences and Technology Society.</p>
+    <div className="center">
+      <div className="whoarewe">
+        <h1>Who Are We?</h1>
+        <img className="us-img" src={process.env.PUBLIC_URL + "/images/US.png"} alt="5 happy girls"></img>
+        <p>From left to right: Marta Masramon Munoz, Mireia Munoz Rojo, Livia Soro, Manuela Giraud, Yacout Rtel Bennani</p>
+      </div>
+
+      <div className="whoarewe yellow">
+          
+         <h4>We are five bioengineering students from Imperial College</h4>
+         <h4>We decided to create Seedling because we wanted to make recycling easier for everyone.</h4>
+         <h4>We created Seedling during a 24h Hackathon hosted by WSET!</h4>
+      </div>
     </div>
 
 
