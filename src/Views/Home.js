045166c6ea80a2e3b01d801a565e03f7b2bd5a82
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 19:42:42 +0000

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -7,7 +7,12 @@ function Home() {
       <div className="home center">
         <h1>Make an impact by recycling!</h1>
         <h2>You recycle. We donate.</h2>
-        <img src={process.env.PUBLIC_URL + "/images/5trees.png"} alt="Little Forest"></img>
+        <img src={process.env.PUBLIC_URL + "/images/BusinessModel.png"} alt="Business Model"></img>
+        <h2>How does it work?</h2>
+        <img src={process.env.PUBLIC_URL + "/images/Image1.png"} alt="Download our app"></img>
+        <img src={process.env.PUBLIC_URL + "/images/Image2.png"} alt="Scan"></img>
+        <img src={process.env.PUBLIC_URL + "/images/Image3.png"} alt="Recycle"></img>
+        <img src={process.env.PUBLIC_URL + "/images/Image4.png"} alt="We donate"></img>
 
         <h2>Download now!</h2>
         <img className="dwn-img1" src={process.env.PUBLIC_URL + "/images/appstore.png"} alt="logo"></img>
