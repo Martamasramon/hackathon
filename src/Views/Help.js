b41e82fd100267ed62df0a61cc9ffd96a9f52c61
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 23:35:24 +0000

    Update Help.js

--- src/Views/Help.js
+++ src/Views/Help.js
@@ -3,13 +3,17 @@ import React from "react";
 
 function Help() {
   return (
-    <div className="help">
+    <div className="Home center">
       <h1>How to recycle</h1>
       <p>Recycling can look intimidating there are different rules for every package. We are here to help you getting it right!</p>
       <img src={process.env.PUBLIC_URL + "/images/5trees.png"} alt="Little Forest"></img>
-      <p>First, you need to know what are the different recycling symbols. It is essential to read them properly so you can do your recycling correctly.</p>
-     <img src={process.env.PUBLIC_URL + "/images/Recycling.png"} alt="Recycling Symbols"></img>
     </div>
+    
+    <div className="home white">
+      <p>First, you need to understand the recycling symbols! It is essential to know them to recycle efficiently.</p>
+    <img src={process.env.PUBLIC_URL + "/images/Recycling.png"} alt="Little Forest"></img>
+      </div>
+    
   );
 }
 
