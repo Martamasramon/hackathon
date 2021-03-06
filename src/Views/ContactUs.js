ceaa6d60baa161851f4c4e48d80e94f02db7d702
marta masramon <marta.masramon@gmail.com>
2021-03-06 17:34:07 +0000

    whoarewe img

--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -3,15 +3,15 @@ import React from "react";
 
 function WhoAreWe() {
   return (
-    <div className="whoarewe">
+    <div className="whoarewe center">
       <h1>Who Are We?</h1>
-       <img src={process.env.PUBLIC_URL + "/images/Pictureofus.png"} alt="5 happy girls"></img>
+       <img className="us-img" src={process.env.PUBLIC_URL + "/images/US.png"} alt="5 happy girls"></img>
        <p>From left to right: Marta Masramon Munoz, Mireia Munoz Rojo, Livia Soro, Manuela Giraud, Yacout Rtel Bennani</p>
       <p>We are five bioengineering students from Imperial College. We decided to create Seedling because we wanted to make recycling easier for everyone.</p>
       <p>We created Seedling during a 24h Hackathon hosted by the Women in Sciences and Technology Society.</p>
     </div>
-    
-    
+
+
   );
 }
 
