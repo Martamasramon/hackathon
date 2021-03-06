00509e55e5504885db842c86481e34e2e45b549d
marta masramon <marta.masramon@gmail.com>
2021-03-06 17:19:32 +0000

    things

--- src/Views/Charities.js
+++ src/Views/Charities.js
@@ -4,18 +4,20 @@ import React from "react";
 function Charities() {
   return (
     <div className="Charities">
-    
-    <div class="charity 1">
+
+    <div className="charity">
     <h2>British Heart Fundation</h2>
+    <img className="char-img" src={process.env.PUBLIC_URL + "/images/heartfoundation.png"} alt="logo"></img>
     <p>British Heart Fundation is funds research related to heart and circulatory diseases.</p>
     </div>
-    
-    <div class="charity 2">
+
+    <div className="charity">
     <h2>Macmillan Cancer support</h2>
+    <img className="char-img" src={process.env.PUBLIC_URL + "/images/mcmillan.png"} alt="logo"></img>
     <p>Macmillan cancer support is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
     </div>
-    
-    
+
+
     </div>
   );
 }
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,14 +4,11 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <div className="home">
+      <div className="home center">
         <h1>Make an impact by recycling!</h1>
         <h2>You recycle. We donate.</h2>
         <img src={process.env.PUBLIC_URL + "/images/5trees.png"} alt="Little Forest"></img>
 
-      </div>
-
-      <div className="download">
         <h2>Download now!</h2>
         <img className="dwn-img1" src={process.env.PUBLIC_URL + "/images/appstore.png"} alt="logo"></img>
         <img className="dwn-img2" src={process.env.PUBLIC_URL + "/images/googleplay.png"} alt="logo"></img>
@@ -20,7 +17,7 @@ function Home() {
 
       <div className="home yellow">
         <h2>How does it work?</h2>
-        <p>Scan the products you want to recycle on our app! Once you have scanned 100 items,and we will donate £2 to the charity of your choice!</p>
+        <p>Scan the products you want to recycle on our app! Once you have scanned 100 items, we will donate £2 to the charity of your choice!</p>
 
       </div>
     </div>
