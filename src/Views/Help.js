af110098b71618628b0cdf541c398a0eaaa12010
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 22:57:42 +0000

    Update Help.js

--- src/Views/Help.js
+++ src/Views/Help.js
@@ -5,7 +5,7 @@ function Help() {
   return (
     <div className="help">
       <h1>How to recycling</h1>
-      <p>Recycling can look intimidating there are different rules for every package. </p>
+      <p>Recycling can look intimidating there are different rules for every package. We are here to help you getting it right!</p>
     </div>
   );
 }
