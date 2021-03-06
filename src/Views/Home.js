8297c604eea2a2a41b81a675d3e38d3effa2cb80
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 19:59:08 +0000

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -10,7 +10,6 @@ function Home() {
         <img src={process.env.PUBLIC_URL + "/images/BusinessModel.png"} alt="Business Model"></img>
         <h2> </h2>
         <img src={process.env.PUBLIC_URL + "/images/5trees.png"} alt="Little Forest"></img>
-        
       </div>
 
 
@@ -20,13 +19,16 @@ function Home() {
         <img src={process.env.PUBLIC_URL + "/images/Image2.png"} alt="Scan"></img>
         <img src={process.env.PUBLIC_URL + "/images/Image3.png"} alt="Recycle"></img>
         <img src={process.env.PUBLIC_URL + "/images/Image4.png"} alt="We donate"></img>
-
+      </div>
+    
+      <div className="home center">
         <h2>Download now!</h2>
         <img className="dwn-img1" src={process.env.PUBLIC_URL + "/images/appstore.png"} alt="logo"></img>
         <img className="dwn-img2" src={process.env.PUBLIC_URL + "/images/googleplay.png"} alt="logo"></img>
-
       </div>
-    </div>
+    
+    
+ 
   );
 }
 
