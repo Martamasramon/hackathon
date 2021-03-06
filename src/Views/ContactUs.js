2d6c449c8ff09cff0c21c2540b7dd2d2a85d7d52
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 17:18:15 +0000

    Update ContactUs.js

--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -5,8 +5,13 @@ function WhoAreWe() {
   return (
     <div className="whoarewe">
       <h1>Who Are We?</h1>
-      <p>Paragraph</p>
+       <img src={process.env.PUBLIC_URL + "/images/Pictureofus.png"} alt="5 happy girls"></img>
+       <p>From left to right: Marta Masramon Munoz, Mireia Munoz Rojo, Livia Soro, Manuela Giraud, Yacout Rtel Bennani</p>
+      <p>We are five bioengineering students from Imperial College. We decided to create Seedling because we wanted to make recycling easier for everyone.</p>
+      <p>We created Seedling during a 24h Hackathon hosted by the Women in Sciences and Technology Society.</p>
     </div>
+    
+    
   );
 }
 
