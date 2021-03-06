0a1f3a0610c7095ab433ed61ead8a2f21c4b6c40
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 22:17:15 +0000

    Update Charities.js

--- src/Views/Charities.js
+++ src/Views/Charities.js
@@ -15,17 +15,38 @@ function Charities() {
       </div>
     </div>
     
-
-    <div class="row">
-      <div class="column">
-        <img className="char-img" src={process.env.PUBLIC_URL + "/mcmillan.png"} alt="logo"></img>
+    <div class="container">
+    <div class="image">
+    <img className="char-img" src={process.env.PUBLIC_URL + "/images/mcmillan.png"} alt="logo"></img>
       </div>
-
-      <div class="column">
-        <h2>Macmillan Cancer Support</h2>
-        <p>Macmillan Cancer Support is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
+      <div class="text">
+        <h3>Macmillan Cancer Support</h3>
+        <p>Macmillan Cancer Support is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer..</p>
       </div>
     </div>
+    
+     <div class="container">
+    <div class="image">
+    <img className="char-img" src={process.env.PUBLIC_URL + "/images/StJohn.png"} alt="logo"></img>
+      </div>
+      <div class="text">
+        <h3>St. John Ambulance</h3>
+        <p>St John Ambulance is a volunteer-led, charitable non-governmental organisation dedicated to the teaching and practice of first aid in England.</p>
+      </div>
+    </div>
+    
+     <div class="container">
+    <div class="image">
+    <img className="char-img" src={process.env.PUBLIC_URL + "/images/Keep.png"} alt="logo"></img>
+      </div>
+      <div class="text">
+        <h3>Keep Britain Tidy</h3>
+        <p>Keep Britain Tidy is a UK-based independent environmental charity. The organisation campaigns to reduce litter, improve local places and prevent waste. It has offices in Wigan and London.</p>
+      </div>
+    </div>
+    
+
+
 
 
     </div>
