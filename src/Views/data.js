7772829d28369f655ba58bfae246c79dccd96c91
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-07 10:29:56 +0000

    Update data.js

--- src/Views/data.js
+++ src/Views/data.js
@@ -63,20 +63,23 @@ module.exports = [
     borough: "Camden",
     card1_title: "Mixed bank",
     card1_body: "To use the banks: you don't need to separate items. Don't use recycling bags - put items straight into the bank.",
-    card2_title: "Clear bag recycling",
-    card2_body: "22 clear recycling bags are distributed per property every three months. If you don’t receive these, please call Streetline on 020 7361 3001."  ,
+
     recycle: [
-      "- Card, cardboard, unshredded paper, newspaper, magazines, junk mail, telephone directories and envelopes",
-      "- Glass containers, bottles and jars (take metal lids off and put them loose in your recycling)",
-      "- Food and drink cartons including Tetra Pak type food and drink cartons",
-      "- Metal containers, tins and cans",
-      "- plastic containers, plastic bottles, pots, tubs and trays"
+      "- Plastic – including carrier bags, bottles, pots, tubs and cartons",
+      "- Paper – including newspapers, magazines and letters",
+      "- Cardboard – including cereal and egg boxes",
+      "- Metal – including tins, cans, aerosols, aluminium foil and bottle tops",
+      "- Glass – including bottles and jars",
+      "Make sure you:",
+      "-rinse and empty all containers",
+      "-remove film lids on plastic pots, tubs and trays",
+      "-keep bottle tops on",
+      "-flatten or fold cardboard boxes",
+      "Do not forget to check your collection day!"
     ],
     noRecycle: [
-      "- Food waste, garden waste, clothes, shoes and textiles",
-      "- Other plastics including carrier bags, polystyrene and cling film",
-      "- Wood, building, DIY materials, foil or metals (other than cans), broken glass and Pyrex glass",
-      "- Shredded paper, nappies including used nappies, light bulbs, batteries and aerosol cans"
+     "Your recycling will not be collected if it contains anything that can’t be recycled. This includes black bin liners, textiles, nappies or food."
+      
     ]
   },
   
