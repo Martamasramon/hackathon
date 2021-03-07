80b5fc4079eaba6ea3daac15e8cd99bb45e2b0b1
martamasramon <marta.masramon@gmail.com>
2021-03-07 00:20:42 +0000

    center home items

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -13,23 +13,23 @@ function Home() {
       </div>
 
 
-      <div className="home yellow">
+      <div className="home yellow center">
         <h2>How does it work?</h2>
         <img src={process.env.PUBLIC_URL + "/images/Image1png.png"} alt="Download our app"></img>
         <img src={process.env.PUBLIC_URL + "/images/Image2.png"} alt="Scan"></img>
         <img src={process.env.PUBLIC_URL + "/images/Imagethatannoysme.png"} alt="Recycle"></img>
         <img src={process.env.PUBLIC_URL + "/images/iimage4.png"} alt="We donate"></img>
       </div>
-    
+
       <div className="home center">
         <h2>Download now!</h2>
         <img className="dwn-img1" src={process.env.PUBLIC_URL + "/images/appstore.png"} alt="logo"></img>
         <img className="dwn-img2" src={process.env.PUBLIC_URL + "/images/googleplay.png"} alt="logo"></img>
       </div>
     </div>
-    
- 
- 
+
+
+
   );
 }
 
