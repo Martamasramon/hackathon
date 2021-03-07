c74fe241a349ca564f6ec1536a6ca83015f9746e
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-07 11:07:39 +0000

    Update ContactUs.js

--- src/Views/ContactUs.js
+++ src/Views/ContactUs.js
@@ -6,12 +6,12 @@ function WhoAreWe() {
     <div className="center">
       <div className="whoarewe">
         <h1>Who Are We?</h1>
-        <img className="us-img" src={process.env.PUBLIC_URL + "/images/US.png"} alt="5 happy girls"></img>
+        <img src={process.env.PUBLIC_URL + "/images/US.png"} width="60" height="40" alt="5 happy girls"></img>
         <p>From left to right: Marta Masramon Munoz, Mireia Munoz Rojo, Livia Soro, Manuela Giraud, Yacout Rtel Bennani</p>
       </div>
 
       <div className="whoarewe green">
-         <img src={process.env.PUBLIC_URL + "/images/tree_sm.png"} alt="logo"></img>
+         <img src={process.env.PUBLIC_URL + "/images/tree_sm.png"} width="60" height="40" alt="logo"></img>
          <h4>We are five bioengineering students from Imperial College. We decided to create Seedling because we wanted to make recycling easier for everyone.</h4>
          <h4>We created Seedling during a 24h Hackathon hosted by WSET!</h4>
       </div>
