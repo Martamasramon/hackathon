a0b51a30fcbc606e08080878ed2f9e98440677d9
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-07 10:51:48 +0000

    Update data.js

--- src/Views/data.js
+++ src/Views/data.js
@@ -63,6 +63,8 @@ module.exports = [
     borough: "Camden",
     card1_title: "Mixed bank",
     card1_body: "To use the banks: you don't need to separate items. Don't use recycling bags - put items straight into the bank.",
+    card2_title: " ",
+    card2_body: "Be sure to put your recycling inside the common recycling bins! .",
 
     recycle: [
       "- Plastic – including carrier bags, bottles, pots, tubs and cartons",
@@ -93,16 +95,16 @@ module.exports = [
   
   {
     borough: "Islington",
-    card1_title: "Mixed bank",
-    card1_body: "To use the banks: you don't need to separate items. Don't use recycling bags - put items straight into the bank.",
-    card2_title: "Clear bag recycling",
-    card2_body: "22 clear recycling bags are distributed per property every three months. If you don’t receive these, please call Streetline on 020 7361 3001.",
+    card1_title: "",
+    card1_body: "Houses and converted flats have weekly recycling collection, while estates have a more regular collections from the communal bins.",
+    card2_title: "Recycling on the go!",
+    card2_body: "Many recycling and litter bins are available accross the borough",
     recycle: [
-      "- Card, cardboard, unshredded paper, newspaper, magazines, junk mail, telephone directories and envelopes",
-      "- Glass containers, bottles and jars (take metal lids off and put them loose in your recycling)",
-      "- Food and drink cartons including Tetra Pak type food and drink cartons",
-      "- Metal containers, tins and cans",
-      "- plastic containers, plastic bottles, pots, tubs and trays"
+      "-Mixed Recycling (Paper, Cardboard, Plastic, Glass, Metal",
+      "-Food waste recycling (Tea bags, food, bread, astry, pet food, coffee ground, etc.",
+      "-Garden waste recycling (leaves, plants, small tree branches, etc.)",
+       "-Small electronical items",
+       "-Textile and clothes"
     ],
     noRecycle: [
       "- Food waste, garden waste, clothes, shoes and textiles",
