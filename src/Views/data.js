b492e5b7a9a22cdfefbc9eff4c317e166b3bdc22
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-07 10:14:10 +0000

    Update data.js

--- src/Views/data.js
+++ src/Views/data.js
@@ -19,26 +19,46 @@ module.exports = [
       "- Shredded paper, nappies including used nappies, light bulbs, batteries and aerosol cans"
     ]
   },
+  
+  
+  
+  
   {
     borough: "Hammersmith and Fulham",
-    card1_title: "Mixed bank",
-    card1_body: "To use the banks: you don't need to separate items. Don't use recycling bags - put items straight into the bank.",
-    card2_title: "Clear bag recycling",
-    card2_body: "22 clear recycling bags are distributed per property every three months. If you don’t receive these, please call Streetline on 020 7361 3001.",
+    card1_title: "Smart Sacks",
+    card1_body: "Smart Sacks are single-use bags used to collect recycling. They are delivered to your own house. You can use them if you live in a house or a small block of flats",
+    card2_title: "Recycling for estates, flats and mansion blocks",
+    card2_body: "Depending on where you live, you can either use Smart Sacks, Recycling chutes, or recycling mini banks.",
     recycle: [
-      "- Card, cardboard, unshredded paper, newspaper, magazines, junk mail, telephone directories and envelopes",
-      "- Glass containers, bottles and jars (take metal lids off and put them loose in your recycling)",
-      "- Food and drink cartons including Tetra Pak type food and drink cartons",
-      "- Metal containers, tins and cans",
-      "- plastic containers, plastic bottles, pots, tubs and trays"
+      "- Mixed glass bottles and jars (take metal lids off and put them loose in your recycling)",
+      "- Unshredded paper, card and flattened cardboard boxes ",
+      "- Plastic bottles (please squash and keep plastic lids on)",
+      "- Plastic pots, tubs and trays and containers",
+      "- Food and drink cartons (please squash and keep plastic lids on)"
+      "- Tetra Paks (please squash and keep plastic lids on)"
+      "- Aluminium and steel tins and cans"
+      "If you are in doubt, don't recycle!"
     ],
     noRecycle: [
-      "- Food waste, garden waste, clothes, shoes and textiles",
-      "- Other plastics including carrier bags, polystyrene and cling film",
-      "- Wood, building, DIY materials, foil or metals (other than cans), broken glass and Pyrex glass",
-      "- Shredded paper, nappies including used nappies, light bulbs, batteries and aerosol cans"
+      "- Food waste",
+      "- Garden waste",
+      "- Plastic wrappers",
+      "- Shredded paper"
+      "- Food-soiled packaging"
+      "- Polystyrene"
+      "- Foil"
+      "- Paper towels"
+      "- Plastic bags/bin bags"
+      "- Textiles, clothes or shoes"
+      "- Aerosol cans"
     ]
   },
+  
+  
+  
+  
+  
+  
   {
     borough: "Camden",
     card1_title: "Mixed bank",
@@ -59,6 +79,15 @@ module.exports = [
       "- Shredded paper, nappies including used nappies, light bulbs, batteries and aerosol cans"
     ]
   },
+  
+  
+  
+  
+  
+  
+  
+  
+  
   {
     borough: "Islington",
     card1_title: "Mixed bank",
